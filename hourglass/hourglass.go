bf21586ae079313b28ba0deb6dfb8d49dad1aea0
Ivan Jijon <ivanjijon@autonomia.digital>
2023-06-17 22:51:44 -0500

    Ticker removed since not necessary, documentation added for exported methods

--- hourglass/hourglass.go
+++ hourglass/hourglass.go
@@ -2,28 +2,29 @@ package hourglass
 
 import "time"
 
+// The Hourglass is the tool we use to measure time lapse
 type Hourglass struct {
-	ticker       *time.Ticker
 	creationTime time.Time
 	d            time.Duration
 	running      bool
 }
 
+// NewHourglass creates an hourglass of a given capacity (duration)
 func NewHourglass(d time.Duration) *Hourglass {
 	return &Hourglass{
 		d: d,
 	}
 }
 
+// Start command sets the moment when the hourglass starts to measure time lapse
 func (h *Hourglass) Start() {
 	// protect with mutex?
 	h.creationTime = time.Now()
-	h.ticker = time.NewTicker(time.Millisecond)
 	h.running = true
 }
 
+// Stop command stops the hourglass
 func (h *Hourglass) Stop() {
-	h.ticker.Stop()
 	h.running = false
 }
 
@@ -38,12 +39,15 @@ func (h *Hourglass) elapsedTime() time.Duration {
 	return 0 * time.Second
 }
 
-func (h *Hourglass) CountDown() time.Duration {
+// Countdown returns the remaining time
+func (h *Hourglass) Countdown() time.Duration {
 	if h.isRunning() {
 		return h.d - h.elapsedTime()
 	}
 	return 0 * time.Second
 }
+
+// Expired returns whether the hourglass time capacity was totally consumed or not
 func (h *Hourglass) Expired() bool {
 	if h.isRunning() {
 		return h.elapsedTime() >= h.d
