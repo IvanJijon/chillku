17e152d4fb24cf0459ff738e583fa91b988d223d
Ivan Jijon <ivanjijon@hotmail.com>
2024-01-20 23:26:32 -0500

    Improved hourglass implementation

--- hourglass/hourglass.go
+++ hourglass/hourglass.go
@@ -1,56 +1,57 @@
 package hourglass
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 // The Hourglass is the tool we use to measure time lapse
 type Hourglass struct {
-	creationTime time.Time
-	capacity     time.Duration
-	running      bool
+	lastStartTime time.Time
+	remainingTime time.Duration
+	isRunning     bool
 }
 
 // NewHourglass creates an hourglass of a given capacity (duration)
 func NewHourglass(d time.Duration) *Hourglass {
 	return &Hourglass{
-		capacity: d,
+		remainingTime: d,
 	}
 }
 
-// Start command sets the moment when the hourglass starts to measure time lapse
-func (h *Hourglass) Start() {
-	// protect with mutex?
-	h.creationTime = time.Now()
-	h.running = true
+// Start command sets the moment when the hourglass (re)starts
+func (h *Hourglass) Start() error {
+	if h.isRunning {
+		return errors.New("cannot start an hourglass that is already running")
+	}
+	h.isRunning = true
+	h.lastStartTime = time.Now()
+	return nil
 }
 
 // Stop command stops the hourglass
-func (h *Hourglass) Stop() {
-	h.running = false
-}
-
-func (h *Hourglass) isRunning() bool {
-	return h.running
+func (h *Hourglass) Stop() error {
+	if !h.isRunning {
+		return errors.New("cannot stop an hourglass that is already stopped")
+	}
+	h.isRunning = false
+	h.remainingTime -= h.elapsedTime()
+	return nil
 }
 
 func (h *Hourglass) elapsedTime() time.Duration {
-	if h.isRunning() {
-		return time.Since(h.creationTime)
-	}
-	return 0 * time.Second
+	return time.Since(h.lastStartTime)
 }
 
-// Countdown returns the remaining time
-func (h *Hourglass) Countdown() time.Duration {
-	if h.isRunning() && h.elapsedTime() < h.capacity {
-		return h.capacity - h.elapsedTime() + time.Second
+// RemainingTime returns the remaining time
+func (h *Hourglass) RemainingTime() time.Duration {
+	if h.remainingTime-h.elapsedTime() > 0 {
+		return (h.remainingTime - h.elapsedTime()).Truncate(time.Second)
 	}
-	return 0 * time.Second
+	return (0 * time.Second).Truncate(time.Second)
 }
 
-// Expired returns whether the hourglass time capacity was totally consumed or not
+// Expired returns whether the hourglass time capacity was totally consumed
 func (h *Hourglass) Expired() bool {
-	if h.isRunning() {
-		return h.elapsedTime() >= h.capacity
-	}
-	return false
+	return h.remainingTime <= 0
 }
